6daf180bc6903a5d9ae907d862a2b1d1f83a8f4c
wcolts2000 <wcolts2000@gmail.com>
2019-10-03 21:48:20 -0700

    added graphql playground

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Link } from 'gatsby'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './footer.css'
 
 const FooterComponent = styled.footer``
 
 const Footer = () => {
+
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          author
+        }
+      }
+    }
+  `)
   return (
     <FooterComponent className="footer-container">
       <div className="footer-right">
@@ -17,9 +27,6 @@ const Footer = () => {
         >
           <FontAwesomeIcon className="footer-icon" icon={['fab', 'github']} />
         </a>
-        <a href="mailto:wcolts2000@gmail.com">
-          <FontAwesomeIcon className="footer-icon" icon="envelope" />
-        </a>
         <a
           href="https://www.linkedin.com/in/sean-pheneger-5393a089/"
           target="_blank"
@@ -28,7 +35,7 @@ const Footer = () => {
           <FontAwesomeIcon className="footer-icon" icon={['fab', 'linkedin']} />
         </a>
       </div>
-      <h4>&copy; 2018 with &#9829; by Sean Pheneger</h4>
+      <h4>&copy; 2018 with &#9829; by {data.site.siteMetadata.author}</h4>
     </FooterComponent>
   )
 }
