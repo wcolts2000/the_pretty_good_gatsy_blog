aa7a828dc27732cb24a16bbcf17054b5ab7d8730
wcolts2000 <wcolts2000@gmail.com>
2019-10-04 11:04:26 -0700

    added markdown support and started posts generation from sourcing filesystem

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,35 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 
 import Layout from '../components/layout'
 
-const Blogs = () => (
-  <Layout>
-    <h1>Blogs and Code Snippets</h1>
-    <p>A compilation of cool techniques and snippets I have found useful</p>
-  </Layout> 
-)
+const Blogs = () => {
+  const data = useStaticQuery(graphql`
+  query {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            title
+            date
+          }
+        }
+      }
+    }
+  }
+  `)
+  return (
+    <Layout>
+      <h1>Blogs and Code Snippets</h1>
+      <p>A compilation of cool techniques and snippets I have found useful</p>
+      <ol>
+        {data.allMarkdownRemark.edges.map((post, i) => (
+          <li key={i} ><h2>{post.node.frontmatter.title} </h2>
+          <p> <date>{post.node.frontmatter.date}</date></p></li>
+        ))} 
+      </ol>
+    </Layout> 
+  )
+}
 
 export default Blogs
---
+++ src/posts/gatsby.md
@@ -0,0 +1,12 @@
+---
+title: "The first blog post from gatbsy"
+date: "2019-10-04"
+---
+
+This is the setup of a markdown blog post
+
+## Topics Covered
+
+1. Gatsby
+1. GrahpQL
+1. React
