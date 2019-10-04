5e887327ed7b4c55f561d4be82a5045fba5c0619
wcolts2000 <wcolts2000@gmail.com>
2019-10-03 22:42:41 -0700

    added 404 page styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import {Link} from 'gatsby';
 import Layout from '../components/layout'
+import lostPageStyles from './404.module.scss'
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <section className={lostPageStyles.wrapper}>
+
+      <h1 className={lostPageStyles.lostHeader}>NOT FOUND</h1>
+      <p className={lostPageStyles.lostPara}>You just hit a route that doesn't exist... the sadness.</p>
+      <Link className={lostPageStyles.lostBtn} to="/" >&nbsp;HOME</Link>
+    </section>
   </Layout>
 )
 
