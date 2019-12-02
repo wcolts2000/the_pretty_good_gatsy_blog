f3561222887b2d1fec64e9ee03e492d07ef0937f
wcolts2000 <wcolts2000@gmail.com>
2019-12-02 15:20:14 -0800

    want to test react flip in production build, added small flip page to technologies component

--- src/pages/technologies.js
+++ src/pages/technologies.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import techStyles from './technologies.module.scss'
+import FlipPage from 'react-flip-page'
 
 import Layout from '../components/layout'
 import Head from '../components/head'
@@ -7,10 +8,18 @@ import Head from '../components/head'
 const Technologies = () => (
   <Layout>
     <Head title="Technologies" />
-    <section className={techStyles.container}>
-      <h1>Various Tech Stacks and Library References and Notes</h1>
-      <p>Here you will find information about various javascript and python libraries and fundamentals</p>
-    </section>
+    <div className={techStyles.flip}>
+      <FlipPage height="430" width="580" orientation="horizontal" pageBackground="#D9B89100">
+        <section className={techStyles.container}>
+          <h1>Various Tech Stacks and Library References and Notes</h1>
+          <p>Here you will find information about various javascript and python libraries and fundamentals</p>
+        </section>
+        <section className={techStyles.container}>
+          <h1>Various Tech Stacks and Library References and Notes</h1>
+          <p>Here you will find information about various javascript and python libraries and fundamentals</p>
+        </section>
+      </FlipPage>
+    </div>
   </Layout>
 )
 
