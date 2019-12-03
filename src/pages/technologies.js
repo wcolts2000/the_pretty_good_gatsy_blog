41349b7aa7891da1e70ec3caebd26b42ed3cd671
wcolts2000 <wcolts2000@gmail.com>
2019-12-02 17:23:14 -0800

    changed component to class to pass component in CDM

--- src/pages/technologies.js
+++ src/pages/technologies.js
@@ -1,31 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
 import techStyles from './technologies.module.scss'
 import FlipPage from 'react-flip-page'
 
 import Layout from '../components/layout'
 import Head from '../components/head'
 
-const Technologies = () => (
-  <Layout>
-    <Head title="Technologies" />
-    <div className={techStyles.flip}>
-      {typeof window === "undefined" ?
-        <div>Loading...</div>
-      :
-        (
-          <FlipPage height="430" width="580" orientation="horizontal" pageBackground="#D9B89100">
-        <section className={techStyles.container}>
-          <h1>Various Tech Stacks and Library References and Notes</h1>
-          <p>Here you will find information about various javascript and python libraries and fundamentals</p>
-        </section>
-        <section className={techStyles.container}>
-          <h1>Various Tech Stacks and Library References and Notes</h1>
-          <p>Here you will find information about various javascript and python libraries and fundamentals</p>
-        </section>
-      </FlipPage>)
-      }}
-    </div>
-  </Layout>
-)
+class Technologies extends Component {
+state = {
+ jsx: ""
+}
+  componentDidMount() {
+    this.setState({jsx: <FlipPage height="430" width="580" orientation="horizontal" pageBackground="#D9B89100">
+    <section className={techStyles.container}>
+      <h1>Various Tech Stacks and Library References and Notes</h1>
+      <p>Here you will find information about various javascript and python libraries and fundamentals</p>
+    </section>
+    <section className={techStyles.container}>
+      <h1>Various Tech Stacks and Library References and Notes</h1>
+      <p>Here you will find information about various javascript and python libraries and fundamentals</p>
+    </section>
+  </FlipPage>})
+  }
+  
+  makeComponent = jsx => {
+    return jsx
+  }
+
+  render() {
+    return (
+      <Layout>
+        <Head title="Technologies" />
+        <div className={techStyles.flip}>
+          {this.makeComponent(this.state.jsx)
+          }
+        </div>
+      </Layout>
+    )
+  }
+}
 
 export default Technologies
