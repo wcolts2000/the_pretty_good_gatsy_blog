a9fa404dc4c048b1d49b86d8511a30f0815f4947
wcolts2000 <wcolts2000@gmail.com>
2019-12-02 20:22:18 -0800

    added mock loader to gatsby-node.js to bypass window object undefined error during build

--- gatsby-node.js
+++ gatsby-node.js
@@ -62,3 +62,18 @@ module.exports.createPages = async ({graphql, actions}) => {
     })
   })
 }
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /react-flip-page/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
--- src/pages/technologies.js
+++ src/pages/technologies.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
 import techStyles from './technologies.module.scss'
-
-
-// import FlipPage from 'react-flip-page'
 import Layout from '../components/layout'
 import Head from '../components/head'
+
+import FlipPage from 'react-flip-page'
 
 class Technologies extends Component {
 state = {
@@ -12,16 +11,16 @@ state = {
 }
   componentDidMount() {
 
-  //   this.setState({jsx: <FlipPage height="430" width="580" orientation="horizontal" pageBackground="#D9B89100">
-  //   <section className={techStyles.container}>
-  //     <h1>Various Tech Stacks and Library References and Notes</h1>
-  //     <p>Here you will find information about various javascript and python libraries and fundamentals</p>
-  //   </section>
-  //   <section className={techStyles.container}>
-  //     <h1>Various Tech Stacks and Library References and Notes</h1>
-  //     <p>Here you will find information about various javascript and python libraries and fundamentals</p>
-  //   </section>
-  // </FlipPage>})
+    this.setState({jsx: <FlipPage height="430" width="580" orientation="horizontal" pageBackground="#D9B89100">
+    <section className={techStyles.container}>
+      <h1>Various Tech Stacks and Library References and Notes</h1>
+      <p>Here you will find information about various javascript and python libraries and fundamentals</p>
+    </section>
+    <section className={techStyles.container}>
+      <h1>Various Tech Stacks and Library References and Notes</h1>
+      <p>Here you will find information about various javascript and python libraries and fundamentals</p>
+    </section>
+  </FlipPage>})
   }
   
   makeComponent = jsx => {
