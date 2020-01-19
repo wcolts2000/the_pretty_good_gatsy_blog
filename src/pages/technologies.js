7eb3e1a764e32aa5573e05e8b8de3cc72fcd4a82
wcolts2000 <wcolts2000@gmail.com>
2020-01-19 11:52:13 -0800

    added a few more channels before calling a break

--- src/pages/technologies.js
+++ src/pages/technologies.js
@@ -25,7 +25,13 @@ const Technologies = () => {
         </div>
         <ResourceCard
           vidUrl="https://www.youtube.com/embed/videoseries?list=PLM_i0obccy3sZGzZdpEtxPcR1iPIV0taQ"
-          topics={['JS', 'Algorithms', 'Frameworks', 'Fundamentals']}
+          topics={[
+            'JS',
+            'Algorithms',
+            'Frameworks',
+            'Fundamentals',
+            'Web Development',
+          ]}
           channelLink="https://www.youtube.com/channel/UCLNgu_OupwoeESgtab33CCw/featured"
           channelName="Coding Garden With CJ"
           summary="CJ is a Senior Engineer that has a growing community of developers
@@ -33,6 +39,39 @@ const Technologies = () => {
           algorithm practice to beginner full stack application building. This
           channel is a wealth of knowledge and is definitely worth a like and
           subscribe!"
+        />
+        <ResourceCard
+          vidUrl="https://www.youtube.com/embed/videoseries?list=PLillGF-RfqbYeckUaD1z6nviTp31GLTH8"
+          topics={[
+            'Languages',
+            'Crash Courses',
+            'Frameworks',
+            'Fundamentals',
+            'Web development',
+          ]}
+          channelLink="https://www.youtube.com/user/TechGuyWeb/featured"
+          channelName="Traversy Media"
+          summary="Brad Traversy has one of the most informative coding channels for beginner to intermediate developers
+          I have come across. He covers nearly everything you could think about with regards to being a web developer, from 
+          fundamentals to frameworks to mental health as a progammer. If you have not subscribed and liked his channel and 
+          videos, don't do another thing before going over and giving his content a look. I also have numerous Udemy courses
+          from him as I love his teaching style and the projects he builds out are fun and have good UIs as well."
+        />
+        <ResourceCard
+          vidUrl="https://www.youtube.com/embed/videoseries?list=PL4-IK0AVhVjMI45H5E9oE47wj-7AZLNnK"
+          topics={[
+            'HTML5',
+            'CSS3',
+            'Pre-processors',
+            'Fundamentals',
+            'Web development',
+          ]}
+          channelLink="https://www.youtube.com/channel/UCJZv4d5rbIKd4QHMPkcABCw"
+          channelName="Kevin Powell"
+          summary="Kevin Powell's channel is an amazing wealth of knowledge regarding styling and front-end design. He
+          covers the basics as well as doing deep dives into some really cool tricks and tips. Anyone looking to step up their
+          front-end game will love the content on his channel. There isn't a person I don't refer to his channel when they ask
+          where to find resources for CSS and front-end."
         />
       </section>
     </Layout>
